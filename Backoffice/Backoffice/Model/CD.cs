c640f5c5b16abd514ad73021d5106110dc9cf0c3
André Almeida <apalmeida1990@gmail.com>
2012-12-17 19:00:44 +0000

    Backoffice 1st commit
    PSIDU

---
+++ Backoffice/Backoffice/Model/CD.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Backoffice.Model
+{
+    class CD
+    {
+        private String name;
+        private List<Music> musicList;
+
+        /************************************************************************/
+        /* Constructors                                                        */
+        /************************************************************************/
+
+        public CD()
+        {
+            this.name = "";
+            this.musicList = new List<Music>();
+        }
+
+        public CD(String _name, List<Music> _music)
+        {
+            this.name = _name;
+            this.musicList = _music;
+        }
+
+        /************************************************************************/
+        /* Modifiers                                                            */
+        /************************************************************************/
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void setName(String _name)
+        {
+            this.name = _name;
+        }
+
+        public void addMusic(Music _music)
+        {
+            this.musicList.Add(_music);
+        }
+
+        public void removeMusic(Music _music)
+        {
+            this.musicList.Remove(_music);
+        }
+
+
+
+
+
+    }
+}
---
+++ Backoffice/Backoffice/Model/Music.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Backoffice.Model
+{
+    class Music
+    {
+        private String name;
+        private String artist;
+        private double price;
+        private int durationInSeconds;
+
+
+        /************************************************************************/
+        /* Constructors                                                        */
+        /************************************************************************/
+
+        public Music()
+        {
+            this.name = "";
+            this.artist = "";
+            this.price = 0;
+            this.durationInSeconds = 0;
+        }
+
+        public Music(String _name, String _artist, double _price, int _durationInSeconds)
+        {
+            this.name = _name;
+            this.artist = _artist;
+            this.price = _price;
+            this.durationInSeconds = _durationInSeconds;
+        }
+        
+        /************************************************************************/
+        /* Modifiers                                                            */
+        /************************************************************************/
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void setName(String _name)
+        {
+            this.name = _name;
+        }
+
+        public String getArtist()
+        {
+            return this.artist;
+        }
+
+        public void setArtist(String _artist)
+        {
+            this.artist = _artist;
+        }
+
+        public double getPrice()
+        {
+            return this.price;
+        }
+
+        public void setName(double _price)
+        {
+            this.price = _price;
+        }
+
+        public int getDuration()
+        {
+
+            return this.durationInSeconds;
+        }
+
+        public void setDuration(int _duration)
+        {
+            this.durationInSeconds = _duration;
+        }
+    }
+}
